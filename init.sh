8442aa9cb7b0db282dd06e7d411c24a9280fc337
wiratmo <4a.wiratmo@gmail.com>
2025-07-12 22:02:43 +0700

    feat: check migrate status and delete migrate

--- init.sh
+++ init.sh
@@ -1,47 +1,44 @@
 #!/bin/bash
 
-set -e  # Exit jika ada error
+set -e  
 set -o pipefail
 
-echo "🟡 [init.sh] Memulai setup container Laravel..."
+
+ENV_FILE=".env"
 
 cd /var/www/html || exit 1
 
+
 echo "⏳ Menunggu koneksi ke database..."
 until php artisan migrate:status > /dev/null 2>&1; do
-  >&2 echo "🕐 Database belum siap... retry dalam 10 detik"
+  >&2 echo "🕐 Plaese exec php artisan migrate and seed (optional)"
   sleep 10
 done
 
-# ====================================
-# 3. Install composer dependencies
-# ====================================
-echo "📦 Menjalankan composer install..."
-composer install --no-dev --optimize-autoloader
-
-# ====================================
-# 4. Set permission folder penting
-# ====================================
-echo "🔐 Mengatur permission folder storage dan cache..."
 chown -R www-data:www-data storage bootstrap/cache
 chmod -R 775 storage bootstrap/cache
 
-# ====================================
-# 5. Artisan commands (safe)
-# ====================================
-echo "⚙️ Artisan commands..."
-php artisan key:generate --force || echo "❌ Gagal generate key"
-php artisan config:clear || true
-php artisan config:cache || true
-php artisan route:cache || true
-php artisan view:cache || true
-php artisan migrate:fresh  || true
-php artisan db:seed || true
+# Pastikan file .env ada
+if [ ! -f "$ENV_FILE" ]; then
+  echo "❌ File .env tidak ditemukan!"
+  exit 1
+fi
 
-echo "✅ Laravel siap diakses di http://localhost:9002 🚀"
+# Ambil nilai APP_KEY dari .env
+APP_KEY=$(grep '^APP_KEY=' "$ENV_FILE" | cut -d '=' -f2-)
 
-# ====================================
-# 6. Jalankan PHP-FPM foreground
-# ====================================
+# Cek apakah APP_KEY kosong atau hanya "APP_KEY="
+if [ -z "$APP_KEY" ]; then
+  echo "🔑 APP_KEY kosong, menjalankan php artisan key:generate..."
+  php artisan key:generate
+else
+  echo "✅ APP_KEY sudah ada, skip generate."
+fi
+
+php artisan config:clear 
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
 exec php-fpm
 
